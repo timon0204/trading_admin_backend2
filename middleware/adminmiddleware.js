02917e150d73d9d5cdb0a24046562a41c05b875f
timon0204 <timonpee60@gmail.com>
2024-08-11 08:45:10 +0300

    'fix_middleware'

--- middleware/adminmiddleware.js
+++ middleware/adminmiddleware.js
@@ -1,4 +1,4 @@
-const { Admin } = require("../models");
+const { Company } = require("../models");
 
 exports.adminmiddleware = async (req, res, next) => {
     const token = req.headers.authorization || "";
@@ -6,8 +6,8 @@ exports.adminmiddleware = async (req, res, next) => {
         res.status(401).json({ state: "No Vailed Token! Please Login Again!" });
         return
     }
-    const admin = await Admin.findOne({ where: { token } });
-    if (!admin) {
+    const company = await Company.findOne({ where: { token } });
+    if (!company) {
         res.status(401).json({ state: "No Vailed Token! Please Login Again!" });
         return
     }
